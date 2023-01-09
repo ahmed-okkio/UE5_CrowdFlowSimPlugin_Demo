eebdf23c4f98e9169d85bf0445e2fa0603903392
Ahmed Hamad <60454980+ahmedhamad-okkio@users.noreply.github.com>
2023-01-09 18:53:05 +0000

    created a test map

---
+++ Source/CrowdFlow_Demo/CrowdFlowExit.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "CrowdFlowExit.h"
+
+// Sets default values
+ACrowdFlowExit::ACrowdFlowExit()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void ACrowdFlowExit::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ACrowdFlowExit::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
